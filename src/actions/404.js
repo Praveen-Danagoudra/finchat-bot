834c8a9b01630728b28be7d7ca6ae93e83e69162
Praveen Danagoudra <praveen.danagoudra@gmail.com>
2020-09-21 21:45:33 +0530

    Initial Botnic commit

---
+++ src/actions/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from '@botonic/react'
+
+export default class extends React.Component {
+	render() {
+		return <Text>I'm not understanding what are you talking about, Please type 'Help'.</Text>
+	}
+}
---
+++ src/actions/help.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from '@botonic/react'
+
+export default class extends React.Component {
+	render() {
+		return (
+		<>
+		<Text>Welcome to FINCHATBOT</Text>
+		<Text>Currently, we can help you to know your account balance. Please type 'check my balance'.</Text>
+		</>)
+	}
+}
